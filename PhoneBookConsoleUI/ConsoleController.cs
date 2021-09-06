717e618ecb4cf1228b59183286a8587d6dd87279
Daniel Aguirre <uhyeay24@gmail.com>
2021-09-06 11:34:02 -0500

    added Account, Contact, ConsoleController, and InputHandler classes. Created IHaveOwner interface for Account/Contact to hold Owner Name variables.

---
+++ PhoneBookConsoleUI/ConsoleController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookConsoleUI
+{
+    public static class ConsoleController
+    {
+        //TODO: Can set up a param for TextAlignment to allow centering the message printed.
+        public enum TextAlignment { Default, Horizontal, VerticalHorizontal }
+
+        /// <summary>
+        /// Print the provided string to the console. If dontEndLine then will use Console.Write, else will WriteLine()
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="dontEndLine"></param>
+        public static void PrintToScreen(string message, bool dontEndLine)
+        {
+            if (dontEndLine)
+            {
+                Console.Write(message);
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+        }
+        /// <summary>
+        /// Print the provided string to the console. If dontEndLine then will use Console.Write, else will WriteLine(). Clear the screen after keypress if clearAfterKeyPress is true.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="dontEndLine"></param>
+        /// <param name="clearAfterKeypress"></param>
+        public static void PrintToScreen(string message, bool dontEndLine, bool clearAfterKeypress)
+        {
+            PrintToScreen(message, dontEndLine);
+            if(clearAfterKeypress)
+            {
+                Clear(true);
+            }
+
+        }
+        
+        public static void PrintToScreen(string[] message)
+        {
+            foreach (var str in message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        /// <summary>
+        /// Print the provided string to the console. If dontEndLine then will use Console.Write, else will WriteLine()
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="dontEndLine"></param>
+        public static void PrintToScreen(string[] message, bool dontEndLine)
+        {
+            if (dontEndLine)
+            {
+                for (int i = 0; i < message.Length; i++)
+                {
+                    // if it's not on the last cycle
+                    if (i != message.Length - 1)
+                    {
+                        Console.WriteLine(message[i]);
+                    }
+                    else
+                    {
+                        Console.Write(message[i]);
+                    }
+                }
+            }
+            else PrintToScreen(message);
+        }
+        /// <summary>
+        /// Print the provided string to the console. If dontEndLine then will use Console.Write, else will WriteLine(). Clear the screen after keypress if clearAfterKeyPress is true.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="dontEndLine"></param>
+        public static void PrintToScreen(string[] message, bool dontEndLine, bool clearAfterKeypress)
+        {
+            PrintToScreen(message, dontEndLine);
+            if(clearAfterKeypress)
+            {
+                Clear(true);
+            }
+        }
+
+
+        /// <summary>
+        /// If true, wait for user to press a key then clear. Else just clear.
+        /// </summary>
+        /// <param name="waitForKeyPress"></param>
+        public static void Clear(bool waitForKeyPress)
+        {
+            if (waitForKeyPress)
+            {
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+                Console.Clear();
+        }
+    }
+}
