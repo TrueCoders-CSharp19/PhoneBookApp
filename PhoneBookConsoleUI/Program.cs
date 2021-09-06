ba7c8792d72553108b2c9232874effe5bf27f5b8
Daniel Aguirre <uhyeay24@gmail.com>
2021-09-06 09:35:48 -0500

    Basic Outline Created

---
+++ PhoneBookConsoleUI/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace PhoneBookConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        #region Basic Features & Requirements
+        /* Goals:       
+            
+            1) Well presented, no bad formatting.
+            2) Should only end when prompted by user, whenever they wish.
+            3) Try to complete the baseline Features by 1 week.
+        */
+        /* Baseline Features:                           
+            
+            1) Create a contact
+            2) Read all the contacts in the phonebook
+            3) Update an existing contact
+            4) Delete a contact
+         */
+        /* Potential Other Features:      
+            
+            1) Saving user contacts to a file like JSON
+            2) Saving the users contacts to a database
+            3) Adding a GUI (Graphical User Interface) to the project
+                        • For Windows apps, you would use win forms or WPF
+                        • For Mac, you would need to research XCode
+            4) Adding an API to do the REST commands from PostMan
+
+        
+            - I don't know anything about 1, 2, or 4, though I'm definitely
+            open to learning about them. WinForms I've played with before. If we want to
+            tackle any of these after the baseline is completed I'm open to that.
+         */
+        /* Our Additional Features:    
+            
+            - Intended to list additional features that we want to include or expand
+            on that are not included in the document. 
+
+            • Search contacts by Contains() with a list that updates as they enter each char.
+            •
+        */
+        #endregion
+
+        #region Project Outline
+        /* Structure
+         
+            How should we begin the project? I'd like to try and make use of abstract classes,
+            method overloading, inheritance, and interfaces. What needs to be set up to begin testing
+            and each tackling a feature?
+
+            Classes to start with for baseline structure:
+
+            • 
+            • 
+
+        
+        */
+        /* Collaboration
+         
+            Once we have finished discussing what the project will be and how we will start
+            we should have a final version of this document with our outline pushed into our
+            main branch on Github. 
+        
+            From there each of us should clone a copy of the project
+            into their own branch on the same repo.
+
+            Each of us can manage our branch however we wish, either in a singular
+            branch or seperating into more branches per feature we work on.
+
+            When a feature is ready to be added to the main project, we can create a new branch
+            to merge both branches into, complete testing and ensure that everyone on the project
+            is good with the code and functionality.
+            
+         */
+        /* Starting Point:
+        
+            Structure has listed what classes we are going to need to begin working. List who
+            is going to begin working with which classes for a starting point. Try to keep it
+            limited to just a few classes to get testing started.
+
+        
+
+        */
+        #endregion
+
+        #region Feature Status:
+
+        /* Instructions:        
+            Here we should list each feature as it is added to the project, one at a time with
+            the owner of that feature listed. 
+        
+            Features should each be their own section so that you can record
+            the history of your feature as you wish to there. Including dates is optional but
+            may help to show your dedication to the project to a potential employer. 
+        
+            Completing dealines is a necessity in the workplace, and this is the opportunity
+            to show an employer how you handle deadlines. However, this is still an optional
+            project so treat it however you want to. This is just a guideline for trying to 
+            make the most out of the project possible.
+         */
+
+        /*Template Feature
+            
+            Feature Owner:
+                
+            Feature Start Date :
+                
+            Feature DeadLine : If you do not assign yourself a deadline you can remove this line.
+                
+            Feature Goal:
+
+        */
+
+
+
+
+        #endregion
+
+    }
+}
